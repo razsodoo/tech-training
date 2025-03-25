65f0ba29da8233029aabb423d9ea7cddf18bec91
razsodoo <razs@odoo.com>
2025-03-25 22:37:35 +0000

    [ADD] New Academy module

---
+++ academy/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Odoo Academy'
+    'summary': """Module to handle Course and Sessions""",
+    'description': """Module to handle:
+        - Courses
+        - Sessions
+        - Attendees
+        """,
+    'license': 'OPL-1'
+    'author': 'fsrs'
+    'website': 'www.odoo.com'
+    'category': 'Custom Modules/Tech Training',
+    'depends': ['base'],
+    'data': [],
+    ' demo': [],
+    'application': True,
+    
+}
