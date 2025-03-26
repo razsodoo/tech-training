891808850a7a260a0af9c3e11d3b8473c5807dc2
razsodoo <razs@odoo.com>
2025-03-26 19:21:09 +0000

    [CREATE] Motorcycle Registry module

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Motorcycle Registry',
+    'summary': "Manage Registration of Motorcycles",
+    'description': """Motorcycle Registry
+    This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycle of the brand.""",
+    'license': 'OPL-1',
+    'author': 'razsodoo',
+    'version': '0.0.1',
+    'website': 'www.github.com/razsodoo/tech-training',
+    'category': 'Kawiil/Custom Modules',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
