7da7e4118d7e4b198169d3e65732ba3f2ea1a213
razsodoo <razs@odoo.com>
2025-03-28 17:03:15 +0000

    [ADD] Quote builder extension module

---
+++ quote_builder_report_bridge/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Custom Quote Report Extension',
+    'version': '1.0',
+    'summary': 'Allows custom reports to be used in place of the default Sale Order report.',
+    'description': 'This module extends the standard Sale Order report by allowing users to select custom reports created with Odoo Studio.',
+    'category': 'Sales',
+    'author': 'RAZS',
+    'license': 'OPL-1',
+    'price': 15,
+    'currency': 'USD',
+    'support': 'ricardozs96@gmail.com',
+    'depends': ['sale', 'base', 'sale_management'],
+    'data': [
+        'views/sale_order_form_view.xml',
+        'views/report_saleorder.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
