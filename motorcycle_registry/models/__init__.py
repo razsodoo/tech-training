109e879be58f1d2bbf988c871d9bce0af48bd605
razsodoo <razs@odoo.com>
2025-05-02 20:28:33 +0000

    [ADD] Course and Motorcicle Registry models to their corresponding modules.

---
+++ motorcycle_registry/models/__init__.py
@@ -0,0 +1,1 @@
+from . import motorcicle_registry
---
+++ motorcycle_registry/models/motorcicle_registry.py
@@ -0,0 +1,15 @@
+from odoo import fields, models
+
+class MotorcicleRegistry(models.Model):
+    _name= 'motorcycle.registry'
+    _description = 'Motorcycle Registry'
+    _rec_name = "registry_number"
+
+    certificate_title = fields.Binary()
+    current_mileage = fields.Float()
+    date_registry = fields.Date()
+    first_name = fields.Char(required=True)
+    last_name = fields.Char(required=True)
+    license_plate = fields.Char()
+    registry_number = fields.Char(required=True)
+    vin = fields.Char(required=True)
