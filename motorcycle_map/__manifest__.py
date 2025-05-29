f7e382ac9a35c93d7dc82cfd199667212e8b0d9f
razsodoo <razs@odoo.com>
2025-05-29 01:38:44 +0000

    Completed map challenge day 9

---
+++ motorcycle_map/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Motorcycle Map View',
+    'version': '1.0',
+    'license': 'OPL-1',
+    'category': 'Custom Modules',
+    'depends': ['motorcycle_registry', 'web_map', 'base_geolocalize'],
+    'data': [
+        'views/motorcycle_map_views.xml',
+    ],
+    'application': False,
+    'installable': True,
+}
